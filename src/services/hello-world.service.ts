08a10a57a493927cf216ee5726577ee20e03e0d3
Patrick Narciso <patrick.ncsilva@gmail.com>
2021-06-17 01:22:43 -0300

    refactor: removes helloWorld files

--- src/services/hello-world.service.ts
+++
@@ -1,7 +0,0 @@
-import {HelloWorldApi} from './hello-world.api';
-export class HelloWorldService implements HelloWorldApi {
-
-  async greeting(name: string = 'World'): Promise<string> {
-    return `Hello, ${name}!`;
-  }
-}
