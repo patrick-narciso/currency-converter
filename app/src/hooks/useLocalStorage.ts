05d8d5837e6335ac29f726f90cecff28359469f6
Patrick Narciso <patrick.ncsilva@gmail.com>
2021-06-17 21:53:52 -0300

    feat(app): creates necessary hooks

---
+++ app/src/hooks/useLocalStorage.ts
@@ -0,0 +1,31 @@
+import { get, off, on, set } from "local-storage";
+import { useCallback, useEffect, useState } from "react";
+
+function useLocalStorage<T>(
+  key: string,
+  initialValue: T
+): [T, (arg0: T) => void] {
+  const [value, setValue] = useState<T>(get(key) ?? initialValue);
+  const setPersistentValue = useCallback(
+    (nextValue: T | ((arg0: T) => T)) => {
+      let newValue = nextValue;
+      if (typeof nextValue === "function") {
+        newValue = (nextValue as (arg0: T) => T)(get(key));
+      }
+      if (set(key, newValue)) {
+        setValue(newValue);
+      }
+    },
+    [key]
+  );
+
+  // On off keep other tabs in sync
+  useEffect(() => {
+    on(key, setValue);
+    return () => off(key, setValue);
+  }, [key, setValue]);
+
+  return [value, setPersistentValue];
+}
+
+export default useLocalStorage;
